c5feb6710f4873072171ad592ae0fb4571dba390
goatfishplays <goatfishplays@gmail.com>
2026-06-28 18:15:41 -0700

    fixed up to make standard windows project, see readme for more details

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WheelTime
 Real Time Application Launcher Styled as a Seletion Wheel
+
+
+# Note For Team
+- Need install cmake, be on windows, and have visual studio Desktop Development with C++
+- Recomend getting the standard cmake extensions on VSCode so can just configure with `ctrl+shift+p` -> `CMake: Configure` -> `CMake: Build`
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,69 @@
-#include "lib.h"
-#include <iostream>
+//
+// #include "lib.h"
+// #include <iostream>
 
-int main()
+// int main()
+// {
+//     // std::cout << "Hello, World\n";
+//     printWorld();
+//     return 0;
+// }
+
+#define UNICODE
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd,
+                            UINT uMsg,
+                            WPARAM wParam,
+                            LPARAM lParam)
 {
-    // std::cout << "Hello, World\n";
-    printWorld();
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI wWinMain(
+    HINSTANCE hInstance,
+    HINSTANCE,
+    PWSTR,
+    int nCmdShow)
+{
+    const wchar_t CLASS_NAME[] = L"MyWindow";
+
+    WNDCLASS wc{};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"Hello World",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        800,
+        600,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr);
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg{};
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
