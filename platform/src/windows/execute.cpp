c07540584659a56106e5c0580b1c3cd2929316cd
hmithil <hmithil@gmail.com>
2026-07-05 21:18:39 -0700

    Add files via upload

---
+++ platform/src/windows/execute.cpp
@@ -0,0 +1,36 @@
+#include <windows.h>
+#include <string>
+#include <iostream>
+
+class AppAction {
+private:
+    std::string applicationPath;
+
+public:
+    AppAction(const std::string& path) : applicationPath(path) {}
+
+    void execute() const {
+        std::cout << "[Action] Launching: " << applicationPath << "\n";
+        ShellExecuteA(NULL, "open", applicationPath.c_str(), NULL, NULL, SW_SHOWNORMAL);
+    }
+};
+
+void onButtonClickHandler(const AppAction& actionToRun) {
+    std::cout << "[UI] Button was clicked by the user.\n";
+    actionToRun.execute();
+}
+
+int main() {
+    AppAction openNotepad("notepad.exe");
+    AppAction openCalculator("calc.exe");
+
+    std::cout << "--- Application Running ---\n\n";
+
+    onButtonClickHandler(openNotepad);
+
+    std::cout << "\n--- Simulating another button click ---\n\n";
+
+    onButtonClickHandler(openCalculator);
+
+    return 0;
+}
