197f77fb1bc87816e6fea242e6d34bec674790e6
Muneeb Syed <muneebsyed987@gmail.com>
2026-07-21 11:30:39 -0700

    Prevent duplicate WheelTime instances

--- launcher/src/main.cpp
+++ launcher/src/main.cpp
@@ -5,6 +5,9 @@
 
 #include <QApplication>
 #include <QCoreApplication>
+#include <QIODevice>
+#include <QLocalServer>
+#include <QLocalSocket>
 #include <App/App.hpp>
 #include <QFile>
 #include <QDebug>
@@ -21,7 +24,52 @@ int main(int argc, char *argv[])
     // Closing Settings (a real Qt::Window) must not quit the hotkey-resident app.
     qtApp.setQuitOnLastWindowClosed(false);
 
+    const QString singleInstanceServerName = "WheelTime.SingleInstance";
+    QLocalSocket existingInstance;
+    existingInstance.connectToServer(singleInstanceServerName, QIODevice::WriteOnly);
+    if (existingInstance.waitForConnected(150))
+    {
+        // WheelTime is normally background-resident. A second exe launch should
+        // act like "bring up settings" instead of creating a second hotkey owner.
+        existingInstance.write("show-settings\n");
+        existingInstance.flush();
+        existingInstance.waitForBytesWritten(150);
+        return 0;
+    }
+
+    QLocalServer::removeServer(singleInstanceServerName);
+    QLocalServer singleInstanceServer;
+    if (!singleInstanceServer.listen(singleInstanceServerName))
+    {
+        qWarning() << "WheelTime is already running or the single-instance server could not start:"
+                   << singleInstanceServer.errorString();
+        return 0;
+    }
+
     Application::App &app = Application::App::getInstance();
+    QObject::connect(&singleInstanceServer, &QLocalServer::newConnection, [&singleInstanceServer, &app]()
+                     {
+                         const auto handleCommand = [&app](QLocalSocket *client)
+                         {
+                             const QByteArray command = client->readAll();
+                             if (command.contains("show-settings"))
+                             {
+                                 app.showSettingsWindow();
+                             }
+                             client->disconnectFromServer();
+                         };
+
+                         while (QLocalSocket *client = singleInstanceServer.nextPendingConnection())
+                         {
+                             QObject::connect(client, &QLocalSocket::readyRead, client, [client, handleCommand]()
+                                              { handleCommand(client); });
+                             QObject::connect(client, &QLocalSocket::disconnected, client, &QLocalSocket::deleteLater);
+                             if (client->bytesAvailable() > 0)
+                             {
+                                 handleCommand(client);
+                             }
+                         }
+                     });
     QFile file(":/styles/defaultWheel.qss");
 
     if (file.open(QFile::ReadOnly))
